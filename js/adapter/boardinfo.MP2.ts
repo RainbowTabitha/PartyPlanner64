19718017fc59d55570df8ebab6f970dcd786fd90
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-01-27 20:09:11 -0600

    Add missing canOverwrites

--- js/adapter/boardinfo.MP2.ts
+++ js/adapter/boardinfo.MP2.ts
@@ -8,140 +8,144 @@ import { toPack } from "../utils/img/ImgPack";
 import { scenes } from "../fs/scenes";
 
 // Western Land - (U) ROM
-const MP2_WESTERN = createBoardInfo("MP2_WESTERN");
-MP2_WESTERN.name = "Western Land";
-MP2_WESTERN.boardDefFile = 64;
-MP2_WESTERN.bgDir = 2;
-MP2_WESTERN.animBgSet = 0;
-MP2_WESTERN.str = {
-  boardSelect: 197,
-  boardNames: [190, 210],
-  boardGreeting: [1248, 1249],
-  boardWinner: 707,
-  boardPlayCount: 1327,
-};
-MP2_WESTERN.img = {
-  introLogoImg: 406,
-  pauseLogoImg: 380,
-  boardSelectImg: 30,
-  boardSelectIconCoords: [39, 20],
-  boardSelectIconMask: 20,
-};
-MP2_WESTERN.mainfsEventFile = [10, 682];
-MP2_WESTERN.mainfsBoardFile = [10, 683];
-MP2_WESTERN.sceneIndex = 0x3E; // 62
-// First I tried 0x0029CF24 / 0x80107C54, but then I included the star event
-// at 0x0029E91C / 0x80?. But then, using items NOP sledded, and I found a mystery
-// table at 0x800DF720 that suggests I should use this actually:
-MP2_WESTERN.eventASMStart = 0xD350; // 0x8010FB50, 0x002A4E20
-MP2_WESTERN.eventASMEnd = 0xEAE4; // 0x801112E4, 0x002A65B4
-// MP2_WESTERN.spaceEventsStartAddr = 0x0011280C;
-// MP2_WESTERN.spaceEventsStartOffset = 0x002A7ADC;
-// MP2_WESTERN.spaceEventsEndOffset = 0x002A7BE0;
-MP2_WESTERN.spaceEventTables = [
-  { upper: 0x33EC, lower: 0x33F4 }, // 0x80105BEC, 0x80105BF4, table 0x80112484
-  { upper: 0x33F8, lower: 0x3400, primary: true }, // 0x80105BF8, 0x80105C00, table 0x8011280C
-  { upper: 0x3404, lower: 0x340C }, // 0x80105C04, 0x80105C0C, table 0x801124EC
-];
-MP2_WESTERN.starSpaceArrOffset = [0xF184, 0xF1F4]; // [0x002A6C54, 0x002A6CC4]; // 0x80111984, 0x801119F4
-MP2_WESTERN.starSpaceCount = 7;
-MP2_WESTERN.toadSpaceArrOffset = [0xF194, 0xF244]; //  [0x002A6C64, 0x002A6D14];
-MP2_WESTERN.bankArrOffset = [0xF388]; // [0x002A6E58]; // 0x80111B88
-MP2_WESTERN.bankCoinArrOffset = [0xF26C]; // [0x002A6D3C]; // 0x80111A6C
-MP2_WESTERN.bankCount = 2;
-MP2_WESTERN.itemShopArrOffset = [0xF38C]; // [0x002A6E5C]; // 0x80111B8C
-MP2_WESTERN.itemShopCount = 1;
-MP2_WESTERN.booArrOffset = [0xF268]; // [0x002A6D38]; // 0x80111A68
-MP2_WESTERN.booCount = 2;
-MP2_WESTERN.audioIndexOffset = 0x33AA; // 0x0029AE7A; // 0x80105BA4
-MP2_WESTERN.onLoad = function(board: IBoard) {
-  board.otherbg.largescene = hvqfs.readBackground(MP2_WESTERN.bgDir + 2).src;
-};
-MP2_WESTERN.onAfterOverwrite = function(board: IBoard) {
-  const sceneView = scenes.getDataView(62);
-  //romView.setUint32(0x29AE18, 0); Baby Bowser
-  //romView.setUint32(0x29AE20, 0); Toad
-  //romView.setUint32(0x29AE28, 0); // Boo assets
-  //romView.setUint32(0x29AE30, 0); // ... not the train
-  sceneView.setUint32(0x3368, 0); // 0x29AE38 // Wipe out wood gates fn, which reads 2 space indices at 2A6DB8
-  //romView.setUint32(0x29AE40, 0); // ... not the train
-  //romView.setUint32(0x29AE48, 0); // ... not the train
-  sceneView.setUint32(0x3380, 0); // 0x29AE50 // Train!
+const MP2_WESTERN = createBoardInfo("MP2_WESTERN", {
+  name: "Western Land",
+  canOverwrite: true,
+  boardDefFile: 64,
+  bgDir: 2,
+  animBgSet: 0,
+  str: {
+    boardSelect: 197,
+    boardNames: [190, 210],
+    boardGreeting: [1248, 1249],
+    boardWinner: 707,
+    boardPlayCount: 1327,
+  },
+  img: {
+    introLogoImg: 406,
+    pauseLogoImg: 380,
+    boardSelectImg: 30,
+    boardSelectIconCoords: [39, 20],
+    boardSelectIconMask: 20,
+  },
+  mainfsEventFile: [10, 682],
+  mainfsBoardFile: [10, 683],
+  sceneIndex: 0x3E, // 62
+  // First I tried 0x0029CF24 / 0x80107C54, but then I included the star event
+  // at 0x0029E91C / 0x80?. But then, using items NOP sledded, and I found a mystery
+  // table at 0x800DF720 that suggests I should use this actually:
+  eventASMStart: 0xD350, // 0x8010FB50, 0x002A4E20
+  eventASMEnd: 0xEAE4, // 0x801112E4, 0x002A65B4
+  // spaceEventsStartAddr: 0x0011280C,
+  // spaceEventsStartOffset: 0x002A7ADC,
+  // spaceEventsEndOffset: 0x002A7BE0,
+  spaceEventTables: [
+    { upper: 0x33EC, lower: 0x33F4 }, // 0x80105BEC, 0x80105BF4, table 0x80112484
+    { upper: 0x33F8, lower: 0x3400, primary: true }, // 0x80105BF8, 0x80105C00, table 0x8011280C
+    { upper: 0x3404, lower: 0x340C }, // 0x80105C04, 0x80105C0C, table 0x801124EC
+  ],
+  starSpaceArrOffset: [0xF184, 0xF1F4], // [0x002A6C54, 0x002A6CC4], // 0x80111984, 0x801119F4
+  starSpaceCount: 7,
+  toadSpaceArrOffset: [0xF194, 0xF244], //  [0x002A6C64, 0x002A6D14],
+  bankArrOffset: [0xF388], // [0x002A6E58], // 0x80111B88
+  bankCoinArrOffset: [0xF26C], // [0x002A6D3C], // 0x80111A6C
+  bankCount: 2,
+  itemShopArrOffset: [0xF38C], // [0x002A6E5C], // 0x80111B8C
+  itemShopCount: 1,
+  booArrOffset: [0xF268], // [0x002A6D38], // 0x80111A68
+  booCount: 2,
+  audioIndexOffset: 0x33AA, // 0x0029AE7A; // 0x80105BA4
 
-  // Prevent unused event table hydration
-  // romView.setUint32(0x29AEBC, 0); // 0x80112484 table
-  // romView.setUint32(0x29AEC0, 0);
-  // romView.setUint32(0x29AEC4, 0);
-  // romView.setUint32(0x29AED4, 0); // 0x801124EC table
-  // romView.setUint32(0x29AED8, 0);
-  // romView.setUint32(0x29AEDC, 0);
+  onLoad: function(board: IBoard) {
+    board.otherbg.largescene = hvqfs.readBackground(MP2_WESTERN.bgDir + 2).src;
+  },
 
-  // Skip Bowser the Brash fight scene
-  // 0x004F is the Bowser scene, 0x0051 is the results scene.
-  // To debug, end game early with 0x800F93AF (turn count)
-  // Then watch scene change 0x800FA63C
-  const sceneEndView = scenes.getDataView(82);
-  sceneEndView.setUint16(0x2E0E, 0x0051); // 0x8010560C, 0x35BBEE
+  onAfterOverwrite: function(board: IBoard) {
+    const sceneView = scenes.getDataView(62);
+    //romView.setUint32(0x29AE18, 0); Baby Bowser
+    //romView.setUint32(0x29AE20, 0); Toad
+    //romView.setUint32(0x29AE28, 0); // Boo assets
+    //romView.setUint32(0x29AE30, 0); // ... not the train
+    sceneView.setUint32(0x3368, 0); // 0x29AE38 // Wipe out wood gates fn, which reads 2 space indices at 2A6DB8
+    //romView.setUint32(0x29AE40, 0); // ... not the train
+    //romView.setUint32(0x29AE48, 0); // ... not the train
+    sceneView.setUint32(0x3380, 0); // 0x29AE50 // Train!
 
-  // Then, make the scared Koopa's message at the endgame be more chill.
-  let bytes: number[] = [];
-  bytes.push(0x0B);
-  bytes = bytes.concat(strings._strToBytes("Don't listen to Toad!"));
-  bytes.push(0x0A); // \n
-  bytes = bytes.concat(strings._strToBytes("I've got the results. Follow me!"));
-  bytes.push(0x00); // Null byte
-  strings.write(697, arrayToArrayBuffer(bytes));
+    // Prevent unused event table hydration
+    // romView.setUint32(0x29AEBC, 0); // 0x80112484 table
+    // romView.setUint32(0x29AEC0, 0);
+    // romView.setUint32(0x29AEC4, 0);
+    // romView.setUint32(0x29AED4, 0); // 0x801124EC table
+    // romView.setUint32(0x29AED8, 0);
+    // romView.setUint32(0x29AEDC, 0);
 
-  // Use the normal character models, not themed.
-  for (let charIdx = 2; charIdx <= 7; charIdx++) {
-    mainfs.write(charIdx, 212, mainfs.get(charIdx, 211)); // Western Land - small model
-    mainfs.write(charIdx, 213, mainfs.get(charIdx, 209)); // Western Land - big model
+    // Skip Bowser the Brash fight scene
+    // 0x004F is the Bowser scene, 0x0051 is the results scene.
+    // To debug, end game early with 0x800F93AF (turn count)
+    // Then watch scene change 0x800FA63C
+    const sceneEndView = scenes.getDataView(82);
+    sceneEndView.setUint16(0x2E0E, 0x0051); // 0x8010560C, 0x35BBEE
+
+    // Then, make the scared Koopa's message at the endgame be more chill.
+    let bytes: number[] = [];
+    bytes.push(0x0B);
+    bytes = bytes.concat(strings._strToBytes("Don't listen to Toad!"));
+    bytes.push(0x0A); // \n
+    bytes = bytes.concat(strings._strToBytes("I've got the results. Follow me!"));
+    bytes.push(0x00); // Null byte
+    strings.write(697, arrayToArrayBuffer(bytes));
+
+    // Use the normal character models, not themed.
+    for (let charIdx = 2; charIdx <= 7; charIdx++) {
+      mainfs.write(charIdx, 212, mainfs.get(charIdx, 211)); // Western Land - small model
+      mainfs.write(charIdx, 213, mainfs.get(charIdx, 209)); // Western Land - big model
+    }
+
+    // For each character (different than loop above)
+    for (let c = 0; c < 6; c++) {
+      // Replace the 2d model renders of themed characters
+      mainfs.write(10, 612 + c, mainfs.get(10, 642 + c));
+
+      // Replace the bowser suited 2d renders
+      mainfs.write(10, 648 + c, mainfs.get(10, 666 + c));
+    }
+
+    // Hide some intro scene graphics
+    // Bowser sign
+    let oldPack = mainfs.get(10, 410);
+    let imgInfoArr = [{ src: new ArrayBuffer(144 * 128 * 4), width: 144, height: 128, bpp: 32 }];
+    let newPack = toPack(imgInfoArr, 16, 0, oldPack);
+    mainfs.write(10, 410, newPack);
+
+    // Hole in ground that Bowser sign sticks into
+    oldPack = mainfs.get(10, 411);
+    imgInfoArr = [{ src: new ArrayBuffer(32 * 16 * 4), width: 32, height: 16, bpp: 32 }];
+    newPack = toPack(imgInfoArr, 16, 0, oldPack);
+    mainfs.write(10, 411, newPack);
+
+    // (unused) Dust in the wind or whatever it is called
+    oldPack = mainfs.get(10, 412);
+    imgInfoArr = [{ src: new ArrayBuffer(64 * 64 * 4), width: 64, height: 64, bpp: 32 }];
+    newPack = toPack(imgInfoArr, 16, 0, oldPack);
+    mainfs.write(10, 412, newPack);
+
+    // Train that rides across, model files can be blanked out
+    // Cannot just blank model, does not work on console.
+    //mainfs.write(10, 394, new ArrayBuffer(0x200));
+    let form = mainfs.get(10, 394);
+    let formView = new DataView(form);
+    formView.setUint16(0x1FC, 0);
+    formView.setUint16(0x1106, 0);
+    mainfs.write(10, 394, form);
+
+    //mainfs.write(10, 395, new ArrayBuffer(0x200));
+    form = mainfs.get(10, 395);
+    formView = new DataView(form);
+    formView.setUint16(0x204, 0);
+    formView.setUint16(0x616, 0);
+    mainfs.write(10, 395, form);
   }
-
-  // For each character (different than loop above)
-  for (let c = 0; c < 6; c++) {
-    // Replace the 2d model renders of themed characters
-    mainfs.write(10, 612 + c, mainfs.get(10, 642 + c));
-
-    // Replace the bowser suited 2d renders
-    mainfs.write(10, 648 + c, mainfs.get(10, 666 + c));
-  }
-
-  // Hide some intro scene graphics
-  // Bowser sign
-  let oldPack = mainfs.get(10, 410);
-  let imgInfoArr = [{ src: new ArrayBuffer(144 * 128 * 4), width: 144, height: 128, bpp: 32 }];
-  let newPack = toPack(imgInfoArr, 16, 0, oldPack);
-  mainfs.write(10, 410, newPack);
-
-  // Hole in ground that Bowser sign sticks into
-  oldPack = mainfs.get(10, 411);
-  imgInfoArr = [{ src: new ArrayBuffer(32 * 16 * 4), width: 32, height: 16, bpp: 32 }];
-  newPack = toPack(imgInfoArr, 16, 0, oldPack);
-  mainfs.write(10, 411, newPack);
-
-  // (unused) Dust in the wind or whatever it is called
-  oldPack = mainfs.get(10, 412);
-  imgInfoArr = [{ src: new ArrayBuffer(64 * 64 * 4), width: 64, height: 64, bpp: 32 }];
-  newPack = toPack(imgInfoArr, 16, 0, oldPack);
-  mainfs.write(10, 412, newPack);
-
-  // Train that rides across, model files can be blanked out
-  // Cannot just blank model, does not work on console.
-  //mainfs.write(10, 394, new ArrayBuffer(0x200));
-  let form = mainfs.get(10, 394);
-  let formView = new DataView(form);
-  formView.setUint16(0x1FC, 0);
-  formView.setUint16(0x1106, 0);
-  mainfs.write(10, 394, form);
-
-  //mainfs.write(10, 395, new ArrayBuffer(0x200));
-  form = mainfs.get(10, 395);
-  formView = new DataView(form);
-  formView.setUint16(0x204, 0);
-  formView.setUint16(0x616, 0);
-  mainfs.write(10, 395, form);
-};
+});
 
 // Pirate Land - (U) ROM
 const MP2_PIRATE = createBoardInfo("MP2_PIRATE");
--- js/adapter/boardinfo.MP3.ts
+++ js/adapter/boardinfo.MP3.ts
@@ -5,88 +5,92 @@ import { scenes } from "../fs/scenes";
 import { BoardType } from "../types";
 
 // Chilly Waters - (U) ROM
-const MP3_CHILLY = createBoardInfo("MP3_CHILLY");
-MP3_CHILLY.name = "Chilly Waters";
-MP3_CHILLY.boardDefFile = 570;
-MP3_CHILLY.bgDir = 3;
-MP3_CHILLY.str = {
-  boardSelect: [
-    [21, 30],
-    [26, 17],
+const MP3_CHILLY = createBoardInfo("MP3_CHILLY", {
+  name: "Chilly Waters",
+  canOverwrite: true,
+  boardDefFile: 570,
+  bgDir: 3,
+  str: {
+    boardSelect: [
+      [21, 30],
+      [26, 17],
+    ],
+    boardGreeting: [24, 12],
+    boardGreetingDuel: [24, 0],
+    boardNames: [
+      [49, 39],
+      [83, 43],
+      // [93, 23], This is the songs list, so we can leave it
+    ],
+  },
+  img: {
+    boardSelectImg: 72,
+    splashLogoImg: 22,
+    splashLogoTextImg: 28,
+    pauseLogoImg: 125,
+    gateImg: 354, // dir 19
+  },
+  sceneIndex: 0x48,
+  mainfsEventFile: [19, 618],
+  mainfsBoardFile: [19, 619],
+  eventASMStart: 0x14AF0, // 0x00330000 // ballpark, but this is wrong -> // 0x0031E814, // is this 0x8011A490 ?
+  eventASMEnd: 0x16BEC, // 0x003320FC, 0x8011C58C
+  // spaceEventsStartAddr: 0x0011E718,
+  // spaceEventsStartOffset: 0x00334288,
+  // spaceEventsEndOffset: 0x18F18, // 0x00334428,
+  spaceEventTables: [
+    { upper: 0x26A8, lower: 0x26B0 }, // 0x80108048, 0x80108050, table 0x8011E2CC
+    { upper: 0x26B4, lower: 0x26BC }, // 0x80108054, 0x8010805C, table 0x8011E718
+    // { upper: 0x31DBD0, lower: 0x31DBD8 }, // 0x80108060, 0x80108068 // This is not a table actually, it is related to the happening spaces
+    { upper: 0x26CC, lower: 0x26D4 }, // 0x8010806C, 0x80108074, table 0x8011E344
+    // A table, but if we remove it Poison Shrooms break and probably other things
+    // { upper: 0x31DBE8, lower: 0x31DBF0 }, // 0x80108078, 0x80108080, table 0x8011E4D8
   ],
-  boardGreeting: [24, 12],
-  boardGreetingDuel: [24, 0],
-  boardNames: [
-    [49, 39],
-    [83, 43],
-    // [93, 23], This is the songs list, so we can leave it
-  ],
-};
-MP3_CHILLY.img = {
-  boardSelectImg: 72,
-  splashLogoImg: 22,
-  splashLogoTextImg: 28,
-  pauseLogoImg: 125,
-  gateImg: 354, // dir 19
-};
-MP3_CHILLY.sceneIndex = 0x48;
-MP3_CHILLY.mainfsEventFile = [19, 618];
-MP3_CHILLY.mainfsBoardFile = [19, 619];
-MP3_CHILLY.eventASMStart = 0x14AF0; // 0x00330000 // ballpark, but this is wrong -> // 0x0031E814; // is this 0x8011A490 ?
-MP3_CHILLY.eventASMEnd = 0x16BEC; // 0x003320FC, 0x8011C58C
-// MP3_CHILLY.spaceEventsStartAddr = 0x0011E718;
-// MP3_CHILLY.spaceEventsStartOffset = 0x00334288;
-// MP3_CHILLY.spaceEventsEndOffset = 0x18F18; // 0x00334428;
-MP3_CHILLY.spaceEventTables = [
-  { upper: 0x26A8, lower: 0x26B0 }, // 0x80108048, 0x80108050, table 0x8011E2CC
-  { upper: 0x26B4, lower: 0x26BC }, // 0x80108054, 0x8010805C, table 0x8011E718
-  // { upper: 0x31DBD0, lower: 0x31DBD8 }, // 0x80108060, 0x80108068 // This is not a table actually, it is related to the happening spaces
-  { upper: 0x26CC, lower: 0x26D4 }, // 0x8010806C, 0x80108074, table 0x8011E344
-  // A table, but if we remove it Poison Shrooms break and probably other things
-  // { upper: 0x31DBE8, lower: 0x31DBF0 }, // 0x80108078, 0x80108080, table 0x8011E4D8
-];
-MP3_CHILLY.starSpaceArrOffset = [0x17910, 0x17980]; // [0x00332E20, 0x00332E90]; // 0x8011D2B0, 0x8011D320
-MP3_CHILLY.starSpaceCount = 8;
-MP3_CHILLY.toadSpaceArrOffset = [0x17920, 0x179DC]; // [0x00332E30, 0x00332EEC]; // 0x8011D2C0, 0x8011D37C
-MP3_CHILLY.bankArrOffset = [0x17B64]; //  [0x00333074]; // 0x8011D504
-MP3_CHILLY.bankCoinArrOffset = [0x17A00]; // [0x00332F10]; // 0x8011D3A0
-MP3_CHILLY.bankCount = 2;
-MP3_CHILLY.itemShopArrOffset = [0x17B68]; // [0x00333078]; // 0x8011D508
-MP3_CHILLY.itemShopCount = 2;
-MP3_CHILLY.booArrOffset = [0x179FC]; // [0x00332F0C]; // 0x8011D39C
-MP3_CHILLY.booCount = 1;
-MP3_CHILLY.gateNeighborsOffset = [0x179D4]; //  [0x00332EE4]; // 0x8011D374
-MP3_CHILLY.gateArrOffset = [0x17A7C]; // [0x00332F8C]; // 0x8011D41C
-MP3_CHILLY.gateCount = 2;
-MP3_CHILLY.arrowRotStartOffset = 0x2398; // 0x0031D8A8; // 0x80107D38
-MP3_CHILLY.arrowRotEndOffset = 0x2440; // 0x0031D950; // 0x80107DDC
-MP3_CHILLY.audioIndexOffset = 0x2682; // 0x0031DB92;
-MP3_CHILLY.onLoad = function(board: IBoard) {
-  board.otherbg.largescene = hvqfs.readBackground(MP3_CHILLY.bgDir + 1).src;
-};
-MP3_CHILLY.onAfterOverwrite = function(board: IBoard) {
-  const sceneView = scenes.getDataView(72);
-  // This code (right inbetween 800EBA60 calls) sets up a function pointer for happening spaces.
-  // Since we don't use any default events, we can overwrite it.
-  sceneView.setUint32(0x26C0, 0); // 0x0031DBD0
-  sceneView.setUint32(0x26C4, 0); // 0x0031DBD4
-  sceneView.setUint32(0x26C8, 0); // 0x0031DBD8 // Could also try to set this to 2484B960, and bump up eventASMStart past 8011A8D8
-  // This current approach only works because of another patch in onAfterSave
+  starSpaceArrOffset: [0x17910, 0x17980], // [0x00332E20, 0x00332E90] // 0x8011D2B0, 0x8011D320
+  starSpaceCount: 8,
+  toadSpaceArrOffset: [0x17920, 0x179DC], // [0x00332E30, 0x00332EEC] // 0x8011D2C0, 0x8011D37C
+  bankArrOffset: [0x17B64], //  [0x00333074] // 0x8011D504
+  bankCoinArrOffset: [0x17A00], // [0x00332F10] // 0x8011D3A0
+  bankCount: 2,
+  itemShopArrOffset: [0x17B68], // [0x00333078] // 0x8011D508
+  itemShopCount: 2,
+  booArrOffset: [0x179FC], // [0x00332F0C] // 0x8011D39C
+  booCount: 1,
+  gateNeighborsOffset: [0x179D4], //  [0x00332EE4] // 0x8011D374
+  gateArrOffset: [0x17A7C], // [0x00332F8C] // 0x8011D41C
+  gateCount: 2,
+  arrowRotStartOffset: 0x2398, // 0x0031D8A8 // 0x80107D38
+  arrowRotEndOffset: 0x2440, // 0x0031D950 // 0x80107DDC
+  audioIndexOffset: 0x2682, // 0x0031DB92
 
-  // Board specific calls
-  //romView.setUint32(0x0031DB0C, 0); // Millenium star?
-  //romView.setUint32(0x0031DB14, 0); // Something else important?
-  //romView.setUint32(0x0031DB1C, 0);
-  //romView.setUint32(0x0031DB24, 0);
-  //romView.setUint32(0x0031DB2C, 0); // Skeleton key doors drawing. (I have a note about 0x80107FC4, YMMV)
-  //romView.setUint32(0x0031DB34, 0); // Bank resources
-  //romView.setUint32(0x0031DB3C, 0); // Item shop assets, draws toad on Z board overview
-  //romView.setUint32(0x0031DB44, 0);
+  onLoad: function(board: IBoard) {
+    board.otherbg.largescene = hvqfs.readBackground(MP3_CHILLY.bgDir + 1).src;
+  },
 
-  // Banish the snowman assets to the dead space.
-  sceneView.setUint16(0x1321E, (board as any)._deadSpace); // 0x0032E72E
-  sceneView.setUint16(0x1323E, (board as any)._deadSpace); // 0x0032E74E
-};
+  onAfterOverwrite: function(board: IBoard) {
+    const sceneView = scenes.getDataView(72);
+    // This code (right inbetween 800EBA60 calls) sets up a function pointer for happening spaces.
+    // Since we don't use any default events, we can overwrite it.
+    sceneView.setUint32(0x26C0, 0); // 0x0031DBD0
+    sceneView.setUint32(0x26C4, 0); // 0x0031DBD4
+    sceneView.setUint32(0x26C8, 0); // 0x0031DBD8 // Could also try to set this to 2484B960, and bump up eventASMStart past 8011A8D8
+    // This current approach only works because of another patch in onAfterSave
+
+    // Board specific calls
+    //romView.setUint32(0x0031DB0C, 0); // Millenium star?
+    //romView.setUint32(0x0031DB14, 0); // Something else important?
+    //romView.setUint32(0x0031DB1C, 0);
+    //romView.setUint32(0x0031DB24, 0);
+    //romView.setUint32(0x0031DB2C, 0); // Skeleton key doors drawing. (I have a note about 0x80107FC4, YMMV)
+    //romView.setUint32(0x0031DB34, 0); // Bank resources
+    //romView.setUint32(0x0031DB3C, 0); // Item shop assets, draws toad on Z board overview
+    //romView.setUint32(0x0031DB44, 0);
+
+    // Banish the snowman assets to the dead space.
+    sceneView.setUint16(0x1321E, (board as any)._deadSpace); // 0x0032E72E
+    sceneView.setUint16(0x1323E, (board as any)._deadSpace); // 0x0032E74E
+  }
+});
 
 // Deep Bloober Sea - (U) ROM
 const MP3_BLOOBER = createBoardInfo("MP3_BLOOBER");
