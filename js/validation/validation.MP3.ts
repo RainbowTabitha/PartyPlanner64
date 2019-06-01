430061fa5bc9246cfd5cee5bb9714c7f5c52a8e5
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-05-31 20:23:06 -0500

    Forgot to validate all MP3 boards the same.

--- js/validation/validation.MP3.ts
+++ js/validation/validation.MP3.ts
@@ -19,23 +19,22 @@ export function getValidationRulesForBoard(gameID: Game, boardIndex: number) {
   const totalArrowsToWrite = getArrowRotationLimit(boardInfo);
   rules.push(getRule("TOOMANYARROWROTATIONS", { limit: totalArrowsToWrite }));
 
-  if (boardIndex === 0) {
-    rules.push(getRule("TOOMANYOFEVENT", { event: BankEvent, high: 2 }));
-    rules.push(getRule("TOOMANYBANKS", { limit: 2 }));
-    rules.push(getRule("TOOMANYOFEVENT", { event: ItemShopEvent, high: 2 }));
-    rules.push(getRule("TOOMANYITEMSHOPS", { limit: 2 }));
-    rules.push(getRule("TOOMANYOFEVENT", { event: BooEvent, high: 1 }));
+  rules.push(getRule("TOOMANYOFEVENT", { event: BankEvent, high: 2 }));
+  rules.push(getRule("TOOMANYBANKS", { limit: 2 }));
+  rules.push(getRule("TOOMANYOFEVENT", { event: ItemShopEvent, high: 2 }));
+  rules.push(getRule("TOOMANYITEMSHOPS", { limit: 2 }));
+  rules.push(getRule("TOOMANYOFEVENT", { event: BooEvent, high: 1 }));
 
-    // TODO: gamemasterplc: @PartyPlanner64 replace 0x323AAC in ROM with a NOP if you want more boos
-    rules.push(getRule("TOOMANYBOOS", { limit: 1 }));
-    rules.push(getRule("TOOMANYGATES", { limit: 2 }));
+  // TODO: gamemasterplc: @PartyPlanner64 replace 0x323AAC in ROM with a NOP if you want more boos
+  rules.push(getRule("TOOMANYBOOS", { limit: 1 }));
+  rules.push(getRule("TOOMANYGATES", { limit: 2 }));
 
-    // TODO: gamemasterplc: if you want to fix being able to pick up stars with few of them @PartyPlanner64 overwrite rom offset 0x320058 with all zeroes
-    rules.push(getRule("BADSTARCOUNT", { low: 8, high: 8 }));
+  // TODO: gamemasterplc: if you want to fix being able to pick up stars with few of them @PartyPlanner64 overwrite rom offset 0x320058 with all zeroes
+  rules.push(getRule("BADSTARCOUNT", { low: 8, high: 8 }));
 
-    // No longer needed? See MP3 onAfterOverwrite.
-    //rules.push(getRule("TOOFEWBLUESPACES", { low: 14 }));
-    //rules.push(getRule("TOOFEWREDSPACES", { low: 1 }));
-  }
+  // No longer needed? See MP3 onAfterOverwrite.
+  //rules.push(getRule("TOOFEWBLUESPACES", { low: 14 }));
+  //rules.push(getRule("TOOFEWREDSPACES", { low: 1 }));
+
   return rules;
 }
