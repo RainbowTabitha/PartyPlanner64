4979acf65eb0fa6cad359160704a917ae6f9705d
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2020-01-04 18:38:12 -0600

    Add more to ultra64.h. Fix alignment of generated functions from C compiler.

--- src/events/includes/ultra64h.ts
+++ src/events/includes/ultra64h.ts
@@ -16,7 +16,236 @@ typedef unsigned short int     u16;
 typedef signed int             s32;
 typedef unsigned int           u32;
 
+// SmallerC can't do u64...
+typedef struct { u32 upper; u32 lower; } u64;
+
 typedef float  f32;
 
-#endif
+#endif /* _ULTRA64_TYPES_H_ */
+
+
+#ifndef _ULTRA64_THREAD_H_
+#define _ULTRA64_THREAD_H_
+
+/* Recommended priorities for system threads */
+#define OS_PRIORITY_MAX      255
+#define OS_PRIORITY_VIMGR    254
+#define OS_PRIORITY_RMON     250
+#define OS_PRIORITY_RMONSPIN 200
+#define OS_PRIORITY_PIMGR    150
+#define OS_PRIORITY_SIMGR    140
+#define OS_PRIORITY_APPMAX   127
+#define OS_PRIORITY_IDLE       0
+
+#define OS_STATE_STOPPED    1
+#define OS_STATE_RUNNABLE    2
+#define OS_STATE_RUNNING    4
+#define OS_STATE_WAITING    8
+
+typedef s32 OSPri;
+typedef s32 OSId;
+
+typedef union
+{
+    struct {
+      f32 f_odd;
+      f32 f_even;
+    } f;
+} __OSfp;
+
+typedef struct
+{
+    /* registers */
+    /*0x20*/  u64 at, v0, v1, a0, a1, a2, a3;
+    /*0x58*/  u64 t0, t1, t2, t3, t4, t5, t6, t7;
+    /*0x98*/  u64 s0, s1, s2, s3, s4, s5, s6, s7;
+    /*0xD8*/  u64 t8, t9, gp, sp, s8, ra;
+    /*0x108*/ u64 lo, hi;
+    /*0x118*/ u32 sr, pc, cause, badvaddr, rcp;
+    /*0x12C*/ u32 fpcsr;
+    __OSfp  fp0,  fp2,  fp4,  fp6,  fp8, fp10, fp12, fp14;
+    __OSfp fp16, fp18, fp20, fp22, fp24, fp26, fp28, fp30;
+} __OSThreadContext;
+
+typedef struct
+{
+    u32 flag;
+    u32 count;
+    u64 time;
+} __OSThreadprofile_s;
+
+typedef struct OSThread_s
+{
+    /*0x00*/ struct OSThread_s *next;
+    /*0x04*/ OSPri priority;
+    /*0x08*/ struct OSThread_s **queue;
+    /*0x0C*/ struct OSThread_s *tlnext;
+    /*0x10*/ u16 state;
+    /*0x12*/ u16 flags;
+    /*0x14*/ OSId id;
+    /*0x18*/ int fp;
+    /*0x1C*/ __OSThreadprofile_s *thprof;
+    /*0x20*/ __OSThreadContext context;
+} OSThread;
+
+void osCreateThread(OSThread *thread, OSId id, void (*entry)(void *), void *arg, void *sp, OSPri pri);
+OSId osGetThreadId(OSThread *thread);
+OSPri osGetThreadPri(OSThread *thread);
+void osSetThreadPri(OSThread *thread, OSPri pri);
+void osStartThread(OSThread *thread);
+void osStopThread(OSThread *thread);
+
+#endif /* _ULTRA64_THREAD_H_ */
+
+
+#ifndef _OS_MESSAGE_H_
+#define	_OS_MESSAGE_H_
+
+typedef u32 OSEvent;
+
+/*
+ * Structure for message
+ */
+typedef void * OSMesg;
+
+/*
+ * Structure for message queue
+ */
+typedef struct OSMesgQueue_s {
+	OSThread	*mtqueue;   /* Queue to store threads blocked on empty mailboxes (receive) */
+	OSThread	*fullqueue; /* Queue to store threads blocked on full mailboxes (send) */
+	s32		validCount;	/* Contains number of valid message */
+	s32		first;		/* Points to first valid message */
+	s32		msgCount;	/* Contains total # of messages */
+	OSMesg		*msg;		/* Points to message buffer array */
+} OSMesgQueue;
+
+/* Events */
+#define OS_EVENT_SW1              0     /* CPU SW1 interrupt */
+#define OS_EVENT_SW2              1     /* CPU SW2 interrupt */
+#define OS_EVENT_CART             2     /* Cartridge interrupt: used by rmon */
+#define OS_EVENT_COUNTER          3     /* Counter int: used by VI/Timer Mgr */
+#define OS_EVENT_SP               4     /* SP task done interrupt */
+#define OS_EVENT_SI               5     /* SI (controller) interrupt */
+#define OS_EVENT_AI               6     /* AI interrupt */
+#define OS_EVENT_VI               7     /* VI interrupt: used by VI/Timer Mgr */
+#define OS_EVENT_PI               8     /* PI interrupt: used by PI Manager */
+#define OS_EVENT_DP               9     /* DP full sync interrupt */
+#define OS_EVENT_CPU_BREAK        10    /* CPU breakpoint: used by rmon */
+#define OS_EVENT_SP_BREAK         11    /* SP breakpoint:  used by rmon */
+#define OS_EVENT_FAULT            12    /* CPU fault event: used by rmon */
+#define OS_EVENT_THREADSTATUS     13    /* CPU thread status: used by rmon */
+#define OS_EVENT_PRENMI           14    /* Pre NMI interrupt */
+
+/* Flags to turn blocking on/off when sending/receiving message */
+#define	OS_MESG_NOBLOCK		0
+#define	OS_MESG_BLOCK		1
+
+extern void		osCreateMesgQueue(OSMesgQueue *, OSMesg *, s32);
+extern s32		osSendMesg(OSMesgQueue *, OSMesg, s32);
+extern s32		osJamMesg(OSMesgQueue *, OSMesg, s32);
+extern s32		osRecvMesg(OSMesgQueue *, OSMesg *, s32);
+extern void		osSetEventMesg(OSEvent, OSMesgQueue *, OSMesg);
+
+#endif /* !_OS_MESSAGE_H_ */
+
+
+#ifndef _OS_CONT_H_
+#define	_OS_CONT_H_
+
+typedef struct {
+	u16     type;    /* Controller Type */
+	u8      status;  /* Controller status */
+	u8      errno;
+} OSContStatus;
+
+typedef struct {
+	u16     button;
+	s8      stick_x; /* -80 <= stick_x <= 80 */
+	s8      stick_y; /* -80 <= stick_y <= 80 */
+	u8      errno;
+} OSContPad;
+
+typedef struct {
+	void    *address;               /* Ram pad Address:  11 bits */
+	u8      databuffer[32];         /* address of the data buffer */
+	u8      addressCrc;             /* CRC code for address */
+	u8      dataCrc;                /* CRC code for data */
+	u8      errno;
+} OSContRamIo;
+
+/* Controller type */
+
+#define CONT_ABSOLUTE           0x0001
+#define CONT_RELATIVE           0x0002
+#define CONT_JOYPORT            0x0004
+#define CONT_EEPROM		0x8000
+#define CONT_EEP16K		0x4000
+#define	CONT_TYPE_MASK		0x1f07
+#define	CONT_TYPE_NORMAL	0x0005
+#define	CONT_TYPE_MOUSE		0x0002
+#define	CONT_TYPE_VOICE		0x0100
+
+/* Controller status */
+
+#define CONT_CARD_ON            0x01
+#define CONT_CARD_PULL          0x02
+#define CONT_ADDR_CRC_ER        0x04
+#define CONT_EEPROM_BUSY	0x80
+
+/* Buttons */
+
+#define CONT_A      0x8000
+#define CONT_B      0x4000
+#define CONT_G	    0x2000
+#define CONT_START  0x1000
+#define CONT_UP     0x0800
+#define CONT_DOWN   0x0400
+#define CONT_LEFT   0x0200
+#define CONT_RIGHT  0x0100
+#define CONT_L      0x0020
+#define CONT_R      0x0010
+#define CONT_E      0x0008
+#define CONT_D      0x0004
+#define CONT_C      0x0002
+#define CONT_F      0x0001
+
+/* Nintendo's official button names */
+
+#define A_BUTTON	CONT_A
+#define B_BUTTON	CONT_B
+#define L_TRIG		CONT_L
+#define R_TRIG		CONT_R
+#define Z_TRIG		CONT_G
+#define START_BUTTON	CONT_START
+#define U_JPAD		CONT_UP
+#define L_JPAD		CONT_LEFT
+#define R_JPAD		CONT_RIGHT
+#define D_JPAD		CONT_DOWN
+#define U_CBUTTONS	CONT_E
+#define L_CBUTTONS	CONT_C
+#define R_CBUTTONS	CONT_F
+#define D_CBUTTONS	CONT_D
+
+/* Controller error number */
+
+#define	CONT_ERR_NO_CONTROLLER		PFS_ERR_NOPACK 		/* 1 */
+#define	CONT_ERR_CONTRFAIL		CONT_OVERRUN_ERROR	/* 4 */
+#define	CONT_ERR_INVALID		PFS_ERR_INVALID		/* 5 */
+#define	CONT_ERR_DEVICE			PFS_ERR_DEVICE 		/* 11 */
+#define	CONT_ERR_NOT_READY		12
+#define	CONT_ERR_VOICE_MEMORY		13
+#define	CONT_ERR_VOICE_WORD		14
+#define	CONT_ERR_VOICE_NO_RESPONSE	15
+
+/* Controller interface */
+
+extern s32 osContInit(OSMesgQueue *, u8 *, OSContStatus *);
+extern s32 osContReset(OSMesgQueue *, OSContStatus *);
+extern s32 osContStartQuery(OSMesgQueue *);
+extern s32 osContStartReadData(OSMesgQueue *);
+extern void osContGetQuery(OSContStatus *);
+extern void osContGetReadData(OSContPad *);
+
+#endif /* _OS_CONT_H_ */
 `;
